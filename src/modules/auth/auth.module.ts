d2abbc5a5530f68b4ecf83e1b6182a8b369e5385
jacksonPrimo <jackson.primo12@gmail.com>
2023-12-09 15:01:39 -0300

    add persistence of data in database in auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TokenUtil } from 'src/utils/token.util';
 import { CryptographyUtil } from 'src/utils/cryptography.util';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [AuthService, TokenUtil, CryptographyUtil],
+  providers: [AuthService, TokenUtil, CryptographyUtil, PrismaService],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,22 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CryptographyUtil } from 'src/utils/cryptography.util';
 import { TokenUtil } from 'src/utils/token.util';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private tokenUtil: TokenUtil,
     private cryptographyUtil: CryptographyUtil,
+    private prisma: PrismaService,
   ) {}
-  async signup(data: any): Promise<any> {
-    data.password = this.cryptographyUtil.encryptPassword(data.password);
-    const tokenCreated = this.tokenUtil.generateToken({ email: data.email });
-
-    return { accessToken: tokenCreated };
+  async signup(params: any): Promise<any> {
+    const encryptPassword = this.cryptographyUtil.encryptPassword(
+      params.password,
+    );
+    const data = {
+      encryptPassword,
+      email: params.email,
+      name: params.name,
+    };
+    try {
+      await this.prisma.user.create({ data });
+      const tokenCreated = this.tokenUtil.generateToken({ email: data.email });
+      return { accessToken: tokenCreated };
+    } catch (e) {
+      console.log(e);
+    }
   }
   async signin(email: string, password: string) {
-    console.log(password);
-    const tokenCreated = this.tokenUtil.generateToken({ email });
+    const user = await this.prisma.user.findFirst({
+      where: { email },
+    });
+    if (!user) {
+      throw new HttpException('password or email invalid', 400);
+    }
+    const passwordIsValid = this.cryptographyUtil.comparePassword(
+      password,
+      user.encryptPassword,
+    );
+    if (!passwordIsValid) {
+      throw new HttpException('password or email invalid', 400);
+    }
+    const tokenCreated = this.tokenUtil.generateToken({ email, id: user.id });
 
     return { accessToken: tokenCreated };
   }
