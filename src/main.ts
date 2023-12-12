793862fe5cb00c99838c15b446a5eca9524fb7df
jacksonPrimo <jackson.primo12@gmail.com>
2023-12-11 21:04:19 -0300

    add mroe configs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  app.enableCors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
